06544c6df71b43fb003bb27ad7d93daf4059f2f2
aritrac <aritrachatterjee2007@gmail.com>
2024-06-22 20:57:47 +0530

    commit-0025

---
+++ P105_IteratorRemoveCollectionRemove/src/IteratorRemoveCollectionRemoveDemo.java
@@ -0,0 +1,8 @@
+public class IteratorRemoveCollectionRemoveDemo {
+    public static void main(String[] args) {
+        /**
+         * Iterator.remove() does not throw ConcurrentModificationException while iterating over a collection
+         * but Collection.remove() method will throw ConcurrentModificationException
+         */
+    }
+}
